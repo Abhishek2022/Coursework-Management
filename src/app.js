213460e083f41a1b417c9d874b9bdbb1fbda30c8
Abhishek2022 <avishekprasad26@gmail.com>
2022-02-09 14:04:23 +0530

    port changed

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const enrollmentRouter = require('./routers/enrollmentRouter')
 const assignmentRouter = require('./routers/assignmentRouter')
 const submissionRouter = require('./routers/submissionRouter')
 
-const port = 3000
+const port = process.env.port || 3000
 
 server.app.use(express.json())
 
